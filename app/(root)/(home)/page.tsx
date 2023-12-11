b8c6f8e07c67bea56c1e2ecff467c3f87e14b2d9
Khomotso <khomotso13@gmail.com>
2023-12-11 19:09:56 +0200

    add local search component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,26 @@
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
-  )
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="max:sm:w-full flex justify-end">
+          <Button
+            className="primary-gradient min-h-[46px] px-4 py-3
+         !text-light-900"
+          >
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between max-sm:flex-col sm:items-center">
+        <LocalSearch  route="/" iconSrc="/assets/icons/search.svg" placeholder="Search Questions" otherClasses="flex-1" iconPosition="left"/>
+
+        Filters
+      </div>
+    </>
+  );
 }

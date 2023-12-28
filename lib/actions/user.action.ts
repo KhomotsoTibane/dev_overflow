3bd5b7e32a23d1fa3108a26ea47b35a099dd6f1a
Khomotso <khomotso13@gmail.com>
2023-12-28 19:42:35 +0200

    comment unused vars

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,7 +63,7 @@ export async function deleteUser(params: DeleteUserParams) {
     // TODO remove user from database, questions, answers, comments...
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
 
     // delete the user questions
     await Question.deleteMany({ author: user._id });

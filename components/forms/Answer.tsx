813b46ce738786015d9631d16978778761e4626b
Khomotso <khomotso13@gmail.com>
2024-05-05 12:42:12 +0200

    generate ai answer completed, insufficient credits

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -27,6 +27,7 @@ interface Props {
 const Answer = ({ question, questionId, authorId }: Props) => {
   const pathname = usePathname();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isGeneratingAi, setIsGeneratingAi] = useState(false);
   const { mode } = useTheme();
   const editorRef = useRef(null);
   const form = useForm<z.infer<typeof AnswerSchema>>({
@@ -59,6 +60,40 @@ const Answer = ({ question, questionId, authorId }: Props) => {
       setIsSubmitting(false);
     }
   };
+
+  // const handleGenerateAiAnswer = async () => {
+  //   if (!authorId) {
+  //     return;
+  //   }
+
+  //   setIsGeneratingAi(true);
+
+  //   try {
+  //     const response = await fetch(
+  //       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify({ question }),
+  //       },
+  //     );
+
+  //     const aiAnswer = await response.json();
+  //     console.log("ai", aiAnswer)
+
+  //     const formattedAnswer = aiAnswer.reply.replace(/\n/g, '<br />');
+
+  //     if (editorRef.current) {
+  //       const editor = editorRef.current as any;
+  //       editor.setContent(formattedAnswer);
+  //     }
+
+  //     alert(aiAnswer.reply);
+  //   } catch (error) {
+  //     console.log("error", error);
+  //   } finally {
+  //     setIsGeneratingAi(false);
+  //   }
+  // };
   return (
     <div>
       <div className="mt-4 flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -69,14 +104,20 @@ const Answer = ({ question, questionId, authorId }: Props) => {
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           onClick={() => {}}
         >
-          <Image
-            src="/assets/icons/stars.svg"
-            alt="star"
-            width={12}
-            height={12}
-            className="object-contain"
-          />
-          Generate an AI Answer
+          {isGeneratingAi ? (
+            <>Generating...</>
+          ) : (
+            <>
+              <Image
+                src="/assets/icons/stars.svg"
+                alt="star"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate an AI Answer
+            </>
+          )}
         </Button>
       </div>
       <Form {...form}>
